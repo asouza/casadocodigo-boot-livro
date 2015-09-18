72ce09b3ae650606bb0fd3d891e331193ffde7a0
asouza <alots.ssa@gmail.com>
2015-09-18 11:53:30 -0300

    removendo import inutil

--- src/main/java/br/com/casadocodigo/loja/controllers/ProductsController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProductsController.java
@@ -3,7 +3,6 @@ package br.com.casadocodigo.loja.controllers;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
