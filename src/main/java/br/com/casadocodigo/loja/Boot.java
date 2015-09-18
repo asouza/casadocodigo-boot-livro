57d8fe54d961ccfa357c229b832db1227ac06ece
asouza <alots.ssa@gmail.com>
2015-09-18 11:18:12 -0300

    configurando compilacao do jsp

--- src/main/java/br/com/casadocodigo/loja/Boot.java
+++ src/main/java/br/com/casadocodigo/loja/Boot.java
@@ -2,9 +2,11 @@ package br.com.casadocodigo.loja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
-public class Boot {
+public class Boot {	
 		
 	public static void main(String[] args) {
 		SpringApplication.run(Boot.class, args);		
